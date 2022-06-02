78a6509e82a03aecdca864e1240b04bebeabb3f9
Reza K Ghazi <itprofessional@hotmail.ca>
2022-06-02 00:36:21 -0400

    Update models.py
    Update link_label field properties.

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,7 +9,7 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
-    link_label = models.CharField(max_length=100, default='')
+    link_label = models.CharField(default='', max_length=100)
 
     def __str__(self):
         return self.title
